7a26b67f20e42a9767f546e3365f18ee512e28da
Ignacio Moran <moran.ua@gmail.com>
2021-06-16 00:40:52 -0400

    traying follow table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     def follow
         @user = User.find(params[:id])
         current_user.followees << @user
-        redirect_back
+        redirect_to root_path
     end
-      
+    
     def unfollow
         @user = User.find(params[:id])
         current_user.followed_users.find_by(followee_id: @user.id).destroy
-        redirect_back
+        redirect_to root_path
     end
 end
